bd94b7010de7f0c3eb587622b969be47dc7401d2
Rainer Jung <rj-atmel@asran.de>
2008-05-25 17:09:00 +0200

    Sequence now triggered by timer

--- LED4/LED4.c
+++ LED4/LED4.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define __DEBUG__
+//#define __DEBUG__
 #ifndef F_CPU
 // define cpu click speed, needed for util/delay.h
 #define F_CPU 3686400
@@ -15,8 +16,24 @@
 
 void init(void);
 void init_ports(void);
+void init_timer(void);
+void next_step(void);
 void out(int);
 int decode(int);
+#ifdef __DEBUG
+void debug(int);
+#endif
+
+// LED-Counter defined globally
+int8_t current_count = 0;
+
+/*
+ * This configures the Timer0 Overflow Interrupt vector to call
+ * next_step.
+ */
+ISR(TIMER0_OVF_vect) {
+  next_step();
+}
 
 /*
  * int main(void) - Main routine
@@ -30,24 +47,20 @@ int decode(int);
  */
 int main(void)
 {
-  // LED-Counter
-  int8_t current_count = 0;
 
   // Initialize ports (later interrupts and so on)
   init();
 
   // Endless loop.
   for(;1==1;) {
+#ifdef __DEBUG__
+    debug(3);
+#endif
     // waiting some time
-    _delay_ms(100);
-    // increasing the counter
-    current_count += 1;
-    // resetting the counter on overflow
-    if(current_count >= (8*4)) {
-      current_count = 0;
-    }
-    // handling of output
-    out(current_count);
+    _delay_ms(1);
+    // call next_step (disabled, as next_step is now called by timer0 
+    // interrupt).
+    //next_step();
   }
 
   // finish process.
@@ -55,12 +68,20 @@ int main(void)
 }
 
 /*
- * void init(void) - Initialize the controller
+ * void init(void) - Initialize ports and timer
  *
  */
 void init(void) {
   // Initialize Ports
   init_ports();
+#ifdef __DEBUG__
+  debug(1);
+#endif
+  // Initialize Timer
+  init_timer();
+#ifdef __DEBUG__
+  debug(2);
+#endif
 }
 
 /*
@@ -82,6 +103,32 @@ void init_ports(void) {
   // Port D
   DDRD = 0xff; // 0 ≘ Input, 1 ≘ Output
   PORTD = 0xff;
+}
+
+/*
+ * void init_timer(void) - Initializes Timer
+ */
+void init_timer(void) {
+  // Enable Timer0 overflow
+  TIMSK = TIMSK | 0x02;
+  // Timer prescaler set to CK/1024
+  TCCR0 = 0x05;
+  // Enable Interrupts
+  sei();
+}
+
+/*
+ * void next_step(void) - Output next step
+ */
+void next_step(void) {
+  // increasing the global counter
+  current_count += 1;
+  // resetting the counter on overflow
+  if(current_count >= (8*4)) {
+    current_count = 0;
+  }
+  // handling of output
+  out(current_count);
 }
 
 /*
@@ -115,17 +162,36 @@ void out(int current) {
  * This just makes 2 ^ in
  */
 int decode(int in) {
-  // i used math.h, but this really just biggened up the hex to 4k.
+  // First i used math.h, but this really just biggened up the hex to 4k.
+  // I think, this is the easiest and fastest way. If you have any
+  // suggestion, please contribute.
   int out = 0;
-  switch(in) {
-    case 0: out = 1; break;
-    case 1: out = 2; break;
-    case 2: out = 4; break;
-    case 3: out = 8; break;
-    case 4: out = 16; break;
-    case 5: out = 32; break;
-    case 6: out = 64; break;
+  switch(in & 0x07) {
+    case 0: out = 1;   break;
+    case 1: out = 2;   break;
+    case 2: out = 4;   break;
+    case 3: out = 8;   break;
+    case 4: out = 16;  break;
+    case 5: out = 32;  break;
+    case 6: out = 64;  break;
     case 7: out = 128; break;
   }
   return out;
 }
+
+#ifdef __DEBUG__
+/*
+ * This debugging-method outputs the code given to Port D.
+ * This can be used for people (like me) that don't have any kind of
+ * hardware debugging possibility to find out, what's happening during
+ * the programm.
+ */
+void debug(int code) {
+  int before = PORTD;
+  PORTD = 0x00;
+  _delay_ms(100);
+  PORTD = code;
+  _delay_ms(200);
+  PORTD = before;
+}
+#endif
